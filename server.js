ffc2922361ecf44edcb5c0eef2451a8289ba5c1b
Hollymm04 <Hollymm04@vt.edu>
2025-04-29 12:40:54 -0400

    added gitignore,  pretty much fixed everything

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3000;
 const path = require('path');
+
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => {
+    console.log('Here')
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
